310f82258f5ebd4245180e91b91b4e3c6cd41680
kn2001 <jniktw@student.agh.edu.pl>
2024-10-08 11:42:47 +0200

    Another Changes

--- first.py
+++ first.py
@@ -1,8 +1,14 @@
 # coding: utf-8
 import matplotlib.pyplot as plt
 import pandas as pd
+"""Read data"""
+dataset = pd.read_csv(u"C:/Users/KN/Downloads/z_geoportal_up42.csv")
 
-dataset = pd.read_csv("pre_proc.csv")
+'''Calculate differences between Z and other height values'''
+dataset=dataset.assign(geoZ = dataset["geoportal1"] - dataset["Z"])
+dataset=dataset.assign(upZ = dataset["up421"] - dataset["Z"])
+
+'''Plotting the results'''
 
 plt.scatter(dataset["X"],dataset["Y"],c=dataset["geoZ"],s=0.01,marker="s",cmap = "seismic")
 plt.title("geoportal1 - Z")
