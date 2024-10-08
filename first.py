22b9966d7316983a8d71696a51b5d8275680543d
kn2001 <jniktw@student.agh.edu.pl>
2024-10-08 12:21:29 +0200

    Another Changes

--- first.py
+++ first.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 import matplotlib.pyplot as plt
 import pandas as pd
+
 """Read data"""
 dataset = pd.read_csv(u"C:/Users/KN/Downloads/z_geoportal_up42.csv")
 
@@ -18,4 +19,9 @@ plt.show()
 plt.scatter(dataset["X"],dataset["Y"],c=dataset["upZ"],s=0.01,marker="s",cmap = "seismic")
 plt.title("up421 - Z")
 plt.colorbar(label="Difference")
+plt.show()
+
+plt.hist(dataset["geoZ"],bins=100)
+plt.show()
+plt.hist(dataset["upZ"],bins=100)
 plt.show()
