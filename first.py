7bbdd5414c2dbfa59a21931f30ad80ec8a23b384
kn2001 <jniktw@student.agh.edu.pl>
2024-10-08 15:28:35 +0200

    asdf

--- first.py
+++ first.py
@@ -1,8 +1,10 @@
 # coding: utf-8
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 """Read data"""
+
 dataset = pd.read_csv(u"C:/Users/KN/Downloads/z_geoportal_up42.csv")
 df = dataset.copy()
 df = df.apply(np.round,args=(2,))
@@ -12,7 +14,6 @@ df.to_pickle("pre_proc_bin")
 '''Read pickled data'''
 
 data = pd.read_pickle("pre_proc_bin")
-
 
 '''Calculate differences between Z and other height values'''
 data=data.assign(geoZ = data["geoportal1"] - data["Z"])
@@ -34,3 +35,8 @@ plt.hist(data["geoZ"],bins=100)
 plt.show()
 plt.hist(data["upZ"],bins=100)
 plt.show()
+
+mae_geoZ = sum(abs(data["geoZ"].fillna(0)))/data.shape[0]
+print(f"MAE (geoportal): {mae_geoZ}")
+mae_upZ = sum(abs(data["upZ"].fillna(0)))/data.shape[0]
+print(f"MAE (up421): {mae_upZ}")
